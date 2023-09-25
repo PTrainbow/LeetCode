092025ff459e2cb3284bfb8c2975f6af62466642
PTrainbow <327365600@qq.com>
2023-09-25 20:04:30 +0800

    feat: Hot100 LeetCode438 找到字符串中所有字母异位词

---
+++ src/LeetCode438.java
@@ -0,0 +1,43 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LeetCode438 {
+    private static int min = Integer.MAX_VALUE;
+
+    public static void main(String[] args) {
+        System.out.println(findAnagrams("abab", "ab"));
+    }
+
+    /**
+     * 竟然没有超时 > 14.27%
+     *
+     * 思路竟然一致，只不过我在 for 循环判断```
+     */
+    public static List<Integer> findAnagrams(String s, String p) {
+        int[] target = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            target[p.charAt(i) - 'a']++;
+        }
+        List<Integer> result = new ArrayList<>();
+        int[] cur = new int[26];
+        for (int i = 0; i <= s.length() - p.length(); i++) {
+            for (int j = i; j < i + p.length(); j++) {
+                cur[s.charAt(j) - 'a']++;
+            }
+            int k = 0;
+            for (k = 0; k < p.length(); k++) {
+                char c = p.charAt(k);
+                if (cur[c - 'a'] != target[c - 'a']) {
+                    break;
+                }
+            }
+            if (k == p.length()) {
+                result.add(i);
+            }
+            Arrays.fill(cur, 0);
+        }
+        return result;
+    }
+}
