2141b447095966b9418313f7d2c4e915cb374d2c
PTrainbow <327365600@qq.com>
2023-11-30 18:55:12 +0800

    feat: Hot100 LeetCode236

--- src/LeetCodeHot100/LeetCode236.java
+++ src/LeetCodeHot100/LeetCode236.java
@@ -2,9 +2,9 @@ package LeetCodeHot100;
 
 public class LeetCode236 {
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1) ;
-        TreeNode node2 = new TreeNode(2) ;
-        TreeNode node3 = new TreeNode(3) ;
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
         node1.left = node2;
         node1.right = node3;
         System.out.println(new Solution().lowestCommonAncestor(node1, node2, node3));
@@ -12,16 +12,27 @@ public class LeetCode236 {
 
 
     private static class Solution {
+
+        /**
+         * > 99.94%
+         */
         public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-            if (root == null || (root == p || root == q)) {
+            return find(root, p, q);
+        }
+
+        private TreeNode find(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return null;
+            }
+            if (root.val == p.val || root.val == q.val) {
                 return root;
             }
-            TreeNode left = lowestCommonAncestor(root.left, p, q);
-            TreeNode right = lowestCommonAncestor(root.right, p, q);
-            if (left != null && right!= null) {
+            TreeNode left = find(root.left, p, q);
+            TreeNode right = find(root.right, p, q);
+            if (left != null && right != null) {
                 return root;
             }
-            return left != null? left: right;
+            return left != null ? left : right;
         }
     }
 
